34cc9bfd0bb49f0c704e222344433ef3aff0bc7b
Jimmy Quiroga <jamesmqy@hotmail.com>
2017-01-02 22:53:28 -0400

    reportes se agrego firma y observaciones

--- admin/code/execute/reporteTplXlsPdf.php
+++ admin/code/execute/reporteTplXlsPdf.php
@@ -6,7 +6,7 @@ $formato =admin::toSql(admin::getParam("type"),"String");
 
 $sql ="SELECT pro_name,pca_name,pro_description,pro_quantity,pro_unidad,sub_status, sub_modalidad, sub_type, sub_hour_end, sub_mount_base, sub_mount_unidad, sub_tiempo, sub_uid 
 FROM mdl_subasta, mdl_product,mdl_pro_category
-WHERE sub_uid=pro_sub_uid and pca_uid=sub_pca_uid and sub_status='ACTIVE' and pro_uid='$pro_uid'";
+WHERE sub_uid=pro_sub_uid and pca_uid=sub_pca_uid and sub_status='ACTIVE' and sub_uid='".$pro_uid."'";
 $db->query($sql);
 while ($firstPart = $db->next_record())
 { 
@@ -25,6 +25,9 @@ while ($firstPart = $db->next_record())
 	$sub_uid=$firstPart['sub_uid'];
 }
 
+$tipoFirma=admin::getDBvalue("SELECT sua_elaborado FROM mdl_subasta_adjudicar where sua_sub_uid='".$sub_uid."'");
+$obs=admin::getDBvalue("SELECT sua_observaciones FROM mdl_subasta_adjudicar where sua_sub_uid='".$sub_uid."'");
+
 $html= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -34,21 +37,21 @@ $html= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://w
 
 <body>
 <table width="100%">
-<tr><td ><img src="../../lib/logo.png" width="100" /></td><td colspan="4"><h1>Parametrizacion</h1><br /><span>Fecha: '.date("d/m/Y").'</span></td></tr>
+<tr><td ><img src="'.$domain.'/lib/logo.png" width="100" /></td><td colspan="4"><h1>Parametrizacion</h1><br /><span>Fecha: '.date("d/m/Y").'</span></td></tr>
 <tr><td><br /><br /></td><td><br /><br /></td></tr>
 <tr><td colspan="5"><h2>1: Datos generales de la subasta</h2></td></tr>
 <tr><td><br /></td><td><br /></td></tr>
-<tr><td width="21%">Nombre:</td><td width="21%" align="left">'.$pro_name.'</td><td width="6%"></td><td width="21%">Cantidad:</td><td width="21%" align="left">'.$pro_quantity.'</td></tr>
-<tr><td width="21%">Categoria:</td><td width="21%" align="left">'.$pca_name.'</td><td width="6%"></td><td width="21%">Unidades:</td><td width="21%" align="left">'.$pro_unidad.'</td></tr>
-<tr><td width="21%">Descripcion:</td><td width="21%"></td><td width="6%"></td><td width="21%"></td><td width="21%"></td></tr>
+<tr><th width="21%">Nombre:</th><td width="21%" align="left">'.$pro_name.'</td><td width="6%"></td><th width="21%">Cantidad:</th><td width="21%" align="left">'.$pro_quantity.'</td></tr>
+<tr><th width="21%">Categoria:</th><td width="21%" align="left">'.$pca_name.'</td><td width="6%"></td><th width="21%">Unidades:</th><td width="21%" align="left">'.$pro_unidad.'</td></tr>
+<tr><th width="21%">Descripcion:</th><td width="21%"></td><td width="6%"></td><td width="21%"></td><td width="21%"></td></tr>
 <tr><td colspan="5" align="left">'.$pro_description.'</td></tr>
 <tr><td><br /><br /></td><td><br /><br /></td></tr>
 <tr><td colspan="5"><h2>2: Datos particulares de la subasta</h2></td></tr>
 <tr><td><br /></td><td><br /></td></tr>
-<tr><td width="26%">Modalidad de subasta:</td><td width="21%" align="left">'.$sub_modalidad.'</td><td width="6%"></td><td width="26%">Fecha de subasta:</td><td width="21%" align="left">'.$sub_hour_end[0].'</td></tr>
-<tr><td width="21%">Tipo de subasta:</td><td width="21%" align="left">'.$sub_type.'</td><td width="6%"></td><td width="21%">Hora de subasta:</td><td width="21%" align="left">'.$sub_hour_end[1].'</td></tr>
-<tr><td width="21%">Monto base:</td><td width="21%" align="left">'.$sub_mount_base.'</td><td width="6%"></td><td width="21%">Tiempo límite de mejora en min.:</td><td width="21%" align="left">'.$sub_tiempo.'</td></tr>
-<tr><td width="21%">Unidad de mejorar:</td><td width="21%" align="left">'.$sub_mount_unidad.'</td><td width="6%"></td><td width="21%"></td><td width="21%"></td></tr>
+<tr><th width="26%">Modalidad de subasta:</th><td width="21%" align="left">'.$sub_modalidad.'</td><td width="6%"></td><th width="26%">Fecha de subasta:</th><td width="21%" align="left">'.$sub_hour_end[0].'</td></tr>
+<tr><th width="21%">Tipo de subasta:</th><td width="21%" align="left">'.$sub_type.'</td><td width="6%"></td><th width="21%">Hora de subasta:</th><td width="21%" align="left">'.$sub_hour_end[1].'</td></tr>
+<tr><th width="21%">Monto base:</th><td width="21%" align="left">'.$sub_mount_base.'</td><td width="6%"></td><th width="21%">Tiempo límite de mejora en min.:</th><td width="21%" align="left">'.$sub_tiempo.'</td></tr>
+<tr><th width="21%">Unidad de mejorar:</th><td width="21%" align="left">'.$sub_mount_unidad.'</td><td width="6%"></td><td width="21%"></td><td width="21%"></td></tr>
 <tr><td><br /><br /></td><td><br /><br a/></td></tr>
 <tr><td colspan="5"><h2>3: Proveedores habilitados</h2></td></tr>
 <tr><td><br /></td><td><br /></td></tr>
@@ -68,10 +71,13 @@ while ($secPart = $db2->next_record())
  }   
 $html.=	'</table>
 </td></tr>
+<tr><td><br /><br /><br /></td><td><br /></td></tr>
+<tr><th colspan="5" align="left">Observaciones:</th></tr>
+<tr><td colspan="5" align="left">'.$obs.'</td></tr>
 <tr><td><br /><br /><br /><br /></td><td><br /><br /><br /><br /></td></tr>
-<tr><td colspan="5" width="100%">
+<tr><td colspan="5">
 	<table width="100%">
-    	<tr><th width="33%" align="center">Elaborado</th><th width="33%" align="center">Revisado</th><th width="33%" align="center">Autorizado</th></tr>
+    	<tr><th width="33%" align="center">Elaborado</th><th width="33%" align="center">Revisado</th><th width="33%" align="center">'.$tipoFirma.'</th></tr>
     </table>
 </td></tr>
 </table>

59713e93029a206ff110d643eeb3fcc57aef9871
Jorge Siles <jorge.siles@gmail.com>
2017-01-14 21:55:31 -0400

    Script update RAV

--- BD/script update20170102.sql
+++ BD/script update20170102.sql
@@ -1,6 +1,6 @@
-insert into sys_modules values (74,'es', 'Configuracion', 0, 'Configuracion', '','', 6, 0, 'ACTIVE', 'subastaRav.php');
-insert into sys_modules values (75,'es', 'Subasta RAV', 74, 'Subasta RAV', '','', 6, 0, 'ACTIVE', 'subastaRav.php');
-insert into sys_modules values (76,'es', 'Subasta RAV Informe', 74, 'Subasta RAV Informe', '','', 6, 0, 'ACTIVE', 'subastaRav1.php');
+insert into sys_modules values (74,'es', 'subastasRav', 0, 'RAV', '','', 8, 0, 'ACTIVE', 'subastasRavList.php');
+insert into sys_modules values (75,'es', 'subastasRavList', 74, 'Listado RAV', '','', 8, 0, 'ACTIVE', 'subastasRavList.php');
+insert into sys_modules values (76,'es', 'subastasRavEdit', 74, 'Editar RAV', '','', 8, 0, 'ACTIVE', 'subastasRavEdit.php');
 insert into sys_modules_users values (74, 1, 'MODULE', 0);
 insert into sys_modules_users values (75, 1, 'MODULE', 0);
 insert into sys_modules_users values (76, 1, 'MODULE', 0);

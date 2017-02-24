a5d1d1d7c1900ab714786c58b873019b5e531197
Jimmy Quiroga <jamesmqy@hotmail.com>
2017-02-24 19:40:26 -0400

    login front estilos

--- code/login.php
+++ code/login.php
@@ -1,15 +1,16 @@
 <div id="content">
 				<div id="box6" class="box-style">
 					<div class="title">
-						<h2><span>Acceso autorizado</span></h2>
+						<h1><span>BANCO NACIONAL DE BOLIVIA S.A. </span></h1><br />
+                        <h2><span>Sistema de Compras </span></h2>
 					</div>
 					<div class="content">
                                             <p>&nbsp;</p>
-						<p>Para ingresar al sitio de Subastas Online, requiere del usuario/correo electrónico y contraseña.</p>
+						<p>Para ingresar al sitio de Subastas Online, requiere del usuario y contraseña.</p>
 						<p>&nbsp;</p>
 						<form name="formLabel" id="formLabel" class="formLabel" autocomplete="off" method="post" action="<?=$domain.'/code/session.php'?>">
 						<p>
-						  <label>Usuario/Correo eletr&oacute;nico:</label><input name="usernameClient" id="usernameClient" type="text" value="" onkeyup="if (event.keyCode==13) document.getElementById('passwordClient').focus();"/> </p><div class="clear"></div>
+						  <label>Usuario:</label><input name="usernameClient" id="usernameClient" type="text" value="" onkeyup="if (event.keyCode==13) document.getElementById('passwordClient').focus();"/> </p><div class="clear"></div>
 						<p><label>Contrase&ntilde;a:</label><input name="passwordClient" id="passwordClient" type="password" value="" onkeyup="if (event.keyCode==13) document.formLabel.submit();"/> </p><div class="clear"></div>
                                                 <p>&nbsp;</p>
                         <a href="#" onclick="document.formLabel.submit();" class="addcart">Ingresar</a>

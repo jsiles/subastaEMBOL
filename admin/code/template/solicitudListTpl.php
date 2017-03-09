9bc58086759a40e584cffbdb1f679751647d273d
jsiles <jorge.siles@gmail.com>
2017-03-08 22:26:18 -0400

    solicitud

--- admin/code/template/solicitudListTpl.php
+++ admin/code/template/solicitudListTpl.php
@@ -215,7 +215,7 @@ while ($sol_list = $pagDb->next_record())
 	<td align="center" width="5%" height="5">
             <?php
             $valuePermit=admin::getDBvalue("select moa_status from sys_modules_options,sys_modules_access where mop_uid=moa_mop_uid and mop_status='ACTIVE'and mop_mod_uid=$moduleListId and mop_lab_category='Eliminar' and moa_rol_uid=".$_SESSION['usr_rol']."");
-            if(($valuePermit=='ACTIVE')&&($solEstadoUid==0)){
+            if(($valuePermit=='ACTIVE')){
             ?>
                 <a href="" onclick="removeList(<?=$solUid?>); return false;">
 		<img src="lib/delete_es.gif" border="0" title="<?=admin::labels('delete')?>" alt="<?=admin::labels('delete')?>">
@@ -233,7 +233,7 @@ while ($sol_list = $pagDb->next_record())
     <div id="status_<?=$solUid?>">
         <?php
             $valuePermit=admin::getDBvalue("select moa_status from sys_modules_options,sys_modules_access where mop_uid=moa_mop_uid and mop_status='ACTIVE'and mop_mod_uid=$moduleListId and mop_lab_category='Estado' and moa_rol_uid=".$_SESSION['usr_rol']."");
-            if(($valuePermit=='ACTIVE')&&($solEstadoUid==0)){
+            if(($valuePermit=='ACTIVE')){
             ?>
 	   <a href="#"  onclick="solicitudCS('<?=$solUid?>','<?=$solStatus?>'); return false;">
 		<img src="<?=admin::labels($labels_content,'linkImage')?>" border="0" title="<?=admin::labels($labels_content)?>" alt="<?=admin::labels($labels_content)?>">

7759d46af4d64d3de97c7dcaea207986f613e54c
Jorge Siles <jorge.siles@gmail.com>
2017-03-17 08:32:11 -0400

    correcciones

--- admin/core/sendMail2.php
+++ admin/core/sendMail2.php
@@ -4,18 +4,18 @@
   require_once "./admin.php";
   set_time_limit(310);
   $mail = new phpmailer();
-  $mail->PluginDir = PLUGINDIR;
-  $mail->Mailer = MAILER;
-  $mail->Host = HOST;
-  $mail->SMTPAuth = SMTPAuth;
-  $mail->Username = USERNAME; 
-  $mail->Password = PASSWORD;
-  $mail->From = FROM;
-  $mail->FromName = FROMNAME;
-  $mail->Timeout=TIMEOUT;
-  $mail->SMTPSecure = SMTPSECURE;
-  $mail->Port=PORT;
-  
+  $mail->PluginDir = "";
+  $mail->Mailer = "smtp";
+  $mail->Host = "mail.sintesis.com.bo";
+  $mail->SMTPAuth = true;
+  $mail->Username = "jorges"; 
+  $mail->Password = "0bje.TIVO";
+  $mail->From = "jorges@sintesis.com.bo";
+  $mail->FromName = "Jorge Siles";
+  $mail->Timeout=300;
+  $mail->SMTPSecure = "";
+  $mail->Port=25;
+   
   $mail->SetLanguage("en", '../phpmailer/language/');
   
   $sSQL="select * from mdl_notificacion_envio where noe_status=0 and noe_retry<=2 and noe_email!=''";
